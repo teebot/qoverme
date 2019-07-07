c41ceab40b02799bdff9a9a84b2063f1935ab97b
teebot <thibaut@cozypixel.com>
2019-07-07 23:41:26 +0200

    Resolve webapp for all get routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,9 @@ app.use(express.json());
 
 app.use("/gql", graphqlApp);
 app.use(express.static(path.join(__dirname, "../webapp")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../webapp/index.html"));
+});
 
 app.listen(process.env.PORT, () =>
   console.log(
