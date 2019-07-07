fb170478215858207bfe6b0f6dac1bc20ac62046
teebot <thibaut@cozypixel.com>
2019-07-07 14:22:32 +0200

    Highlight new quote

--- webapp/src/components/Results.tsx
+++ webapp/src/components/Results.tsx
@@ -7,17 +7,30 @@ import {
   Table,
   TableHead,
   TableRow,
-  TableCell
+  TableCell,
+  TableBody,
+  makeStyles,
+  Theme
 } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  selected: {
+    background: theme.palette.primary.light
+  }
+}));
+
 export default function Results() {
+  const classes = useStyles();
   const { data, error, loading } = useQuery(RESULTS_QUERY);
   if (loading) {
-    return <div>Loading... ⏳</div>;
+    return <div>Loading...</div>;
   }
 
   if (error || !data) {
     return <div>Oops something went wrong (눈⁠_⁠눈)</div>;
   }
+
+  const selected = window.location.hash.substring(1);
 
   const quotes: Quote[] = data.quotes;
   return (
@@ -31,17 +44,21 @@ export default function Results() {
             <TableCell>Car Purchase Price</TableCell>
           </TableRow>
         </TableHead>
-        {quotes.map((quote, i) => (
-          <>
-            <a id={quote.id.toString()} />
-            <TableRow key={i}>
+        <TableBody>
+          {quotes.map((quote, i) => (
+            <TableRow
+              key={i}
+              className={
+                selected && quote.id === selected ? classes.selected : ""
+              }
+            >
               <TableCell>{quote.age}</TableCell>
               <TableCell>{quote.plan}</TableCell>
               <TableCell>{quote.carBrand}</TableCell>
               <TableCell>{quote.carPurchasePrice}</TableCell>
             </TableRow>
-          </>
-        ))}
+          ))}
+        </TableBody>
       </Table>
     </Paper>
   );
--- webapp/src/types/quote.type.ts
+++ webapp/src/types/quote.type.ts
@@ -1,3 +1,3 @@
 import { QuoteParams } from "./quote-params.type";
 
-export type Quote = QuoteParams & { id: number; plan: string };
+export type Quote = QuoteParams & { id: string; plan: string };
